ae1a4382201a9e2f4ffc685d5994d19e1bc7b959
Eda Deniz <edeniz9191@gmail.com>
2020-09-01 14:36:51 -0400

    Updates

---
+++ precache-manifest.c24c1442aac7ad67bb9ab4d078e31540.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6345f75f0e3a57a6b3be5975e35f873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11ee96629497d15e5904",
+    "url": "/static/css/main.afbf6ed4.chunk.css"
+  },
+  {
+    "revision": "f003cc099fa89fbdfb65",
+    "url": "/static/js/2.bc6b92f4.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/static/js/2.bc6b92f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ee96629497d15e5904",
+    "url": "/static/js/main.73496fcb.chunk.js"
+  },
+  {
+    "revision": "1487e38b14c5de2a7dd1",
+    "url": "/static/js/runtime-main.a77d6087.js"
+  },
+  {
+    "revision": "e372779eca5a9c2b4ef3bdbb2a397c9a",
+    "url": "/static/media/bootstraplogo.e372779e.png"
+  },
+  {
+    "revision": "f6108efd045ebcd489345bc22964bb86",
+    "url": "/static/media/chai.f6108efd.png"
+  },
+  {
+    "revision": "8316e471d267b903f454ec34e1100296",
+    "url": "/static/media/jasmine.8316e471.png"
+  },
+  {
+    "revision": "1980bf31017b5b59083aa006d153bad1",
+    "url": "/static/media/mindpop.1980bf31.png"
+  },
+  {
+    "revision": "d42672d4d185739d26257ed5c653c740",
+    "url": "/static/media/nodelogo.d42672d4.png"
+  },
+  {
+    "revision": "626e00da60634a0d039d64319c01705a",
+    "url": "/static/media/oauth.626e00da.png"
+  },
+  {
+    "revision": "d3704b5fb15d6602f94f506e075d3def",
+    "url": "/static/media/pandemicessentials.d3704b5f.png"
+  },
+  {
+    "revision": "08b791daad3b4dae318483788479f87a",
+    "url": "/static/media/postico.08b791da.png"
+  },
+  {
+    "revision": "bb71d7d21d35128c3adbd53041a3d4b7",
+    "url": "/static/media/profpic.bb71d7d2.png"
+  },
+  {
+    "revision": "fbaa87b8ec097d6b64164ca6328bc915",
+    "url": "/static/media/sequelize.fbaa87b8.png"
+  },
+  {
+    "revision": "a06840bac3acdd7cad8d57cdd3550de7",
+    "url": "/static/media/socket-io-logo.a06840ba.png"
+  },
+  {
+    "revision": "7cd7253e1639eb935416a1d160934323",
+    "url": "/static/media/webpack.7cd7253e.png"
+  }
+]);
